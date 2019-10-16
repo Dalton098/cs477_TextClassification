3f3a35b76ac612400e437015805cab64269b95d0
Dalton098 <Dalton098@ymail.com>
2019-10-16 13:18:21 -0400

    Initialize

---
+++ classification.py
@@ -0,0 +1,48 @@
+from keras.preprocessing import sequence
+from keras.models import Sequential
+from keras.layers import Dense, Embedding, Flatten, Dropout, LSTM, GRU
+from keras.datasets import imdb
+import numpy as np
+
+max_features = 500
+maxlen=50
+batch_size = 32
+
+# Fix for loading in the data is from stack overflow
+# save np.load
+np_load_old = np.load
+
+# modify the default parameters of np.load
+np.load = lambda *a,**k: np_load_old(*a, allow_pickle=True, **k)
+
+# call load_data with allow_pickle implicitly set to true
+(x_train, y_train), (x_test, y_test) = imdb.load_data(num_words=max_features)
+
+# restore np.load for future normal usage
+np.load = np_load_old
+
+x_train = sequence.pad_sequences(x_train, maxlen=maxlen)
+x_test = sequence.pad_sequences(x_test, maxlen=maxlen)
+
+model = Sequential()
+
+model.add(Embedding(max_features, 128, input_length=maxlen))
+model.add(GRU(128))
+model.add(Dense(1, activation='sigmoid'))
+
+model.compile(loss='binary_crossentropy',
+              optimizer='adam',
+              metrics=['accuracy'])
+
+print('Train...')
+
+model.fit(x_train, y_train,
+          batch_size=batch_size,
+          epochs=15,
+          validation_data=(x_test, y_test))
+
+score, acc = model.evaluate(x_test, y_test,
+                            batch_size=batch_size)
+
+print('Test score:', score)
+print('Test accuracy:', acc)
