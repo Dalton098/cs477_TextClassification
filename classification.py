11ca577fda39569af9646eb114364bcdd2dd0e28
Dalton <dalton098@ymail.com>
2019-10-17 12:35:07 -0400

    Version used to run test

--- classification.py
+++ classification.py
@@ -1,25 +1,27 @@
 from keras.preprocessing import sequence
 from keras.models import Sequential
-from keras.layers import Dense, Embedding, Flatten, Dropout, LSTM, GRU
+from keras.layers import Dense, Embedding, Flatten, Dropout, LSTM, GRU, Recurrent, SimpleRNN
+from keras.layers import CuDNNGRU, CuDNNLSTM
 from keras.datasets import imdb
 import numpy as np
 
-max_features = 500
-maxlen=50
+
+max_features = 20000
+maxlen=80
 batch_size = 32
 
 # Fix for loading in the data is from stack overflow
 # save np.load
-np_load_old = np.load
+# np_load_old = np.load
 
 # modify the default parameters of np.load
-np.load = lambda *a,**k: np_load_old(*a, allow_pickle=True, **k)
+# np.load = lambda *a,**k: np_load_old(*a, allow_pickle=True, **k)
 
 # call load_data with allow_pickle implicitly set to true
 (x_train, y_train), (x_test, y_test) = imdb.load_data(num_words=max_features)
 
 # restore np.load for future normal usage
-np.load = np_load_old
+# np.load = np_load_old
 
 x_train = sequence.pad_sequences(x_train, maxlen=maxlen)
 x_test = sequence.pad_sequences(x_test, maxlen=maxlen)
@@ -27,7 +29,9 @@ x_test = sequence.pad_sequences(x_test, maxlen=maxlen)
 model = Sequential()
 
 model.add(Embedding(max_features, 128, input_length=maxlen))
-model.add(GRU(128))
+# model.add(GRU(128))
+model.add(SimpleRNN(128))
+model.add(Dropout(0.2))
 model.add(Dense(1, activation='sigmoid'))
 
 model.compile(loss='binary_crossentropy',
@@ -36,9 +40,11 @@ model.compile(loss='binary_crossentropy',
 
 print('Train...')
 
-model.fit(x_train, y_train,
+epochs = 15
+
+history = model.fit(x_train, y_train,
           batch_size=batch_size,
-          epochs=15,
+          epochs=epochs,
           validation_data=(x_test, y_test))
 
 score, acc = model.evaluate(x_test, y_test,
@@ -46,3 +52,4 @@ score, acc = model.evaluate(x_test, y_test,
 
 print('Test score:', score)
 print('Test accuracy:', acc)
+
--- data.md
+++ data.md
@@ -1,5 +1,30 @@
-|                                    | Accuracy | Loss   |
+**Baseline**
+
+|                                    | Accuracy | Score  |
 | ---------------------------------- | -------- | ------ |
-| **Bag of Words (Batch size = 32)** | 0.679    | 2.3010 |
-|                                    |          |        |
+| **Bag of Words (Batch size = 32)** | 0.6791   | 2.3010 |
+
+### Recurrent Models
+
+**Max Features = 500, Max Length = 50**
+
+|                                                | Accuracy | Score  |
+| ---------------------------------------------- | -------- | ------ |
+| **GRU (Batch size = 32)**                      | 0.7545   | 0.7570 |
+| **GRU (Batch size = 32, Dropout = 0.2)**       | 0.7594   | 0.6843 |
+| **LSTM (Batch size = 32)**                     | 0.7596   | 0.6081 |
+| **LSTM (Batch size = 32, Dropout = 0.2)**      | 0.7592   | 0.5905 |
+| **SimpleRNN (Batch size = 32)**                | 0.6862   | 0.5875 |
+| **SimpleRNN (Batch size = 32, Dropout = 0.2)** | 0.6546   | 0.6095 |
+
+**Max Features = 20,000, Max Length = 80**
+
+|                                                | Accuracy | Score  |
+| ---------------------------------------------- | -------- | ------ |
+| **GRU (Batch size = 32)**                      | 0.8071   | 1.5070 |
+| **GRU (Batch size = 32, Dropout = 0.2)**       | 0.8142   | 1.2643 |
+| **LSTM (Batch size = 32)**                     | 0.8115   | 1.0614 |
+| **LSTM (Batch size = 32, Dropout = 0.2)**      | 0.8059   | 1.0915 |
+| **SimpleRNN (Batch size = 32)**                | 0.734    | 0.6682 |
+| **SimpleRNN (Batch size = 32, Dropout = 0.2)** | 0.6522   | 0.6218 |
 
